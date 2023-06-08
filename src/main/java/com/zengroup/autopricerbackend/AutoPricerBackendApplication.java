179c85c42f85b119414080943dfb96437db2a3b3
Mariano <marianogermanguerrero@gmail.com>
2023-06-08 04:55:00 -0300

    First Commit Initializr Project Generation

---
+++ src/main/java/com/zengroup/autopricerbackend/AutoPricerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.zengroup.autopricerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoPricerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoPricerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zengroup/autopricerbackend/AutoPricerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zengroup.autopricerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoPricerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
